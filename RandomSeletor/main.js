3c5c7baec5ff0e615f1312c0d5152c386b5b1283
MFPKChenPo <gary199722@gmail.com>
2019-06-16 01:08:46 +0800

    final

--- RandomSeletor/main.js
+++ RandomSeletor/main.js
@@ -1,8 +1,15 @@
+var pic = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg"
+]
+
 $(document).ready(function () {
     $("input").click(function () 
     {
         var numOfListItem = $("#choices li").length;
         var randomChildNum = Math.floor(Math.random()*numOfListItem);
         $("H1").text($("#choices li").eq(randomChildNum).text());
+        $("img").attr("src", pic[randomChildNum]);
     });
 });
